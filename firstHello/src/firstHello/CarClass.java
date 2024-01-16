5e82dd7666673a9787efb463d7f9282561459504
skuser2010 <sasukey2010@yahoo.com>
2024-01-16 19:08:15 +0530

    Carclass added

---
+++ firstHello/src/firstHello/CarClass.java
@@ -0,0 +1,15 @@
+package firstHello;
+
+public class CarClass {
+	
+	private String model;
+	private String priceRange;
+	private String doors;
+	
+	public void stateOfCar() {
+		
+		System.out.println("your car model is: "+ model);
+		System.out.println("your car's price range is : "+ priceRange);
+	}
+
+}
